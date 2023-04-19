c3512ef02140e6b90917dc2ca25f6aaa55537d11
Leonardo Ferreira <leoepferreira@hotmail.com>
2023-04-19 00:01:47 -0300

    First Upload

---
+++ src/main/java/com/myprog/firstapp/FirsappApplication.java
@@ -0,0 +1,13 @@
+package com.myprog.firstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirsappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprog/firstapp/FirsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprog.firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
